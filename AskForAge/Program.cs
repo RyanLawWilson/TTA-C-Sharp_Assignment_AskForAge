46286e6975aa5fc8d5ae730c1d3d6bc7bbd8f107
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-03-06 16:06:13 -0700

    Add project files.

---
+++ AskForAge/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using RLWUtil;  // On GitHub.  Just makes printing and reading user input cooler.  Just has Fancy.cs right now.
+
+namespace AskForAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fancy.Write("Hello");
+            Console.Read();
+        }
+    }
+}
